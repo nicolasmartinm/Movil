dd347def4860e9b75c71d9ad354dba8cb7b3c786
david s <svr160801@gmail.com>
2025-03-05 00:06:00 -0500

    MOVIL_LIST

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'FIREBASE_WEB_APIKEY',
+  databaseURL: 'https://carpetas-22bc3-default-rtdb.firebaseio.com/',
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: false,
+})
+export class AppComponent {
+
+  
+  constructor() {
+    
+    initializeApp(firebaseConfig);
+  }
+}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { getDatabase, onValue, ref, remove } from "firebase/database";
+
+const db = getDatabase();
+const dbRef = ref(db, 'infos/');
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  async delete(id: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure want to delete this info?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('cancel');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            const deleteRef = ref(db, 'infos/' + id);
+            remove(deleteRef);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  infos: any[] = [];
+
+  constructor(private router: Router, private loadingController: LoadingController, private alertController: AlertController) {
+    onValue(dbRef, (snapshot) => {
+      this.infos = [];
+      snapshot.forEach((childSnapshot) => {
+        this.infos.push({
+          id: childSnapshot.key,
+          info_title: childSnapshot.val().info_title,
+          info_description: childSnapshot.val().info_description
+        });
+      });
+    }, {
+      onlyOnce: false
+    });
+  }
+  
+   addInfo() {
+     this.router.navigate(['/add-info']);
+  }
+
+  edit(id: string) {
+    this.router.navigate(['/edit', { id: id }]);
+  }
+
+  
+
+}
+
